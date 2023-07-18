dd4b3ef39a2c599828033310cf27c640fb1423af
GustavoMacias <gustavo.maci.1234@gmail.com>
2023-07-18 14:35:00 -0600

    crud utilizando server-json, mi propia API, tambien usando async

---
+++ Curso JS Moderno/44-PROYECTO-CRM-CRUD-REST/js/editarcliente.js
@@ -0,0 +1,62 @@
+import { obtenerCliente, editarCliente } from "./API.js";
+import { mostrarAlerta, validar } from './funciones.js'
+(function () {
+
+    //campos del formulario 
+
+    const nombreInput = document.querySelector('#nombre')
+    const emailInput = document.querySelector('#email')
+    const empresaInput = document.querySelector('#empresa')
+    const telefonoInput = document.querySelector('#telefono')
+    const idInput = document.querySelector('#id')
+    document.addEventListener("DOMContentLoaded", async () => {
+        const parmetrosURL = new URLSearchParams(window.location.search);
+        const idCliente = parseInt(parmetrosURL.get('id'))
+        const cliente = await obtenerCliente(idCliente);
+
+
+        mostrarCliente(cliente)
+
+        //submit al formulario
+
+        const formulario = document.querySelector('#formulario')
+        formulario.addEventListener('submit', validarCliente)
+    })
+
+    function mostrarCliente(cliente) {
+        console.log(cliente)
+        const { nombre, empresa, email, telefono, id } = cliente
+
+        nombreInput.value = nombre
+        empresaInput.value = empresa
+        emailInput.value = email
+        telefonoInput.value = telefono
+        idInput.value = id
+
+    }
+
+    function validarCliente(e) {
+        e.preventDefault()
+
+
+        const cliente = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+
+
+        if (validar(cliente)) {
+            //Mostrar mensaje
+            mostrarAlerta("todos los campos son obligatorios")
+            return;
+        }
+
+        // rescribe el objeto
+        editarCliente(cliente)
+
+    }
+})()
