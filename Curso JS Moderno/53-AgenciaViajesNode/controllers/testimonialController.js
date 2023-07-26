024a389ac39aeb97aab4f0b4e8b0d041a7c31828
GustavoMacias <gustavo.maci.1234@gmail.com>
2023-07-26 12:35:39 -0600

    Introduccion a node, proyendo completo usando node

---
+++ Curso JS Moderno/53-AgenciaViajesNode/controllers/testimonialController.js
@@ -0,0 +1,65 @@
+import { Testimonial } from "../models/testimoniales.js"
+
+const guardarTestimonial = async (req, res) => {
+
+    //validar...
+
+    const { nombre, correo, mensaje } = req.body
+
+    const errores = []
+
+    if (nombre.trim() === '') {
+        errores.push({ mensaje: 'El Nombre esta vacio' })
+    }
+
+    if (correo.trim() === '') {
+        errores.push({ mensaje: 'El correo esta vacio' })
+    }
+
+    if (mensaje.trim() === '') {
+        errores.push({ mensaje: 'El Mensaje esta vacio' })
+    }
+
+    if (errores.length > 0) {
+
+        try {
+            //Consultar Testimoniales existentes
+
+
+            const testimoniales = await Testimonial.findAll()
+            //Mostrar la vista con errores
+            res.render("testimoniales", {
+                pagina: "Testimoniales",
+                errores,
+                nombre,
+                correo,
+                mensaje,
+                testimoniales
+            })
+        } catch (error) {
+            console.log(error)
+        }
+
+    } else {
+        //Almacenarlo en la base de datos
+
+        try {
+            console.log(mensaje)
+            await Testimonial.create({
+                nombre,
+                correo,
+                mensaje
+
+            })
+            res.redirect('/testimoniales')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+}
+
+
+export {
+    guardarTestimonial
+}
---
+++ Curso JS Moderno/53-AgenciaViajesNode/index.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import router from './routes/index.js';
+import db from './config/db.js';
+
+
+
+// conectar a la base de datos
+db.authenticate()
+    .then(() => console.log('base de datos conectada'))
+    .catch(error => console.log(error))
+
+//Definir puerto
+const port = process.env.PORT || 4000
+
+
+//Habilitar pug
+app.set('view engine', 'pug')
+
+// Obtener el año actual
+app.use((req, res, next) => {
+    const year = new Date()
+    res.locals.actualYear = year.getFullYear()
+    res.locals.nombresitio = "Agencia de Viajes"
+
+    next()
+})
+
+
+// Agregar body parser para leer los datos del formulario
+
+app.use(express.urlencoded({ extended: true }));
+
+
+//Definir la carpeta publica
+app.use(express.static('public'))
+
+
+//Agregar router
+
+app.use('/', router)
+
+app.listen(port, () => {
+    console.log(`Server is running in the port ${port}`);
+})
